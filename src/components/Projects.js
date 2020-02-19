ed777be388382d773a12f125e21351b5e43efbe2
Khoa Thien Le (Harry) <46258781+khoa165@users.noreply.github.com>
2020-02-18 23:54:07 -0600

    Update description for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,7 @@ import '../styles/Projects.scss';
 const projects = [
   {title: 'BuddE Network', image: "https://res.cloudinary.com/khoa165/image/upload/q_100/v1577895922/portfolio/friends.svg",
   descriptions: [
-    'Mini social network that allows creating/removing users, adding/removing friendship between users, navigating through the network from the view of each user.',
+    'Built a social network visualization that allows creating/removing users, adding/removing friendship between users, navigating through the network from the view of each user.',
     'Led a team of four to complete a decent final class project. Facilitated coding sessions to build prototype with Lucidchart and assist others with desiging, implementing and debugging the program.',
     'Set up, configured, and structured the project. Managed the coding progress throughout three weeks span. Implemented interfaces, graph data structure and the majority of front end.'
   ],
@@ -13,23 +13,23 @@ const projects = [
   source: "https://github.com/khoa165/BuddENetwork"},
   {title: 'Buddy Up', image: "https://res.cloudinary.com/khoa165/image/upload/q_100/v1577895922/portfolio/buddies.svg",
   descriptions: [
-    'MVP platform to connect people with similar interests, focus on 1-on-1 buddy networking.',
-    'Pitched idea and convinced others to join team. Led a team of four with proficient workflow through GitHub collaboration platform. Split user stories across team members, built intuitive UX/UI.'
+    'Built a minimum viable product using Ruby on Rails and PostgreSQL that connects people with similar interests, focusing on 1-on-1 buddy networking and using a criteria-weighted score system to process user data.',
+    'Pitched idea and convinced others to join team. Led a team of four students using GitHub for collaboration and adopting Agile methodology to achieve an efficient 2-week sprint: daily standup and in-person interactions, Trello board for user stories/features-oriented, and flexible course of actions to ensure working software.'
   ],
   skills: ['Ruby on Rails', 'PostgreSQL', 'HTML | CSS | JS', 'Heroku', 'GitHub', 'Bootstrap', 'jQuery', 'Cloudinary', 'Devise authentication', 'Geocoding', 'PredictHQ API', 'Figma'],
   demo: "https://www.buddyup.xyz/",
   source: "https://github.com/khoa165/buddyUp"},
   {title: 'Fish Evolution', image: "https://res.cloudinary.com/khoa165/image/upload/q_100/v1577895922/portfolio/shark.svg",
   descriptions: [
-    'Classic game of big fish capturing smaller fish.',
-    'Practiced Object-oriented programming (OOP) concepts and Data Structures and Algorithms (DSA).'
+    'Built a classic game of big fish capturing smaller fish to evolve using Java and Processing graphic library',
+    'Practiced Object-oriented programming concepts and project design.'
   ],
   skills: ['Back-end with Java', 'Front-end with Processing'],
   source: "https://github.com/khoa165/fish-evolution"},
   {title: 'Pet Now', image: "https://res.cloudinary.com/khoa165/image/upload/q_100/v1577895922/portfolio/pet.svg",
   descriptions: [
-    'MVP platform to allow pet lover to rent pet for a short amount of time.',
-    'Pitched idea and convinced others to join team. Led a team of four with proficient workflow through GitHub collaboration platform. Split user stories across team members, built intuitive UX/UI.'
+    'Built a minimum viable product using Ruby on Rails and PostgreSQL that connects pet lovers and those who might be in need of having others take care of their pets.',
+    'Pitched idea and convinced others to join team. Led a team of four students using GitHub for collaboration and adopting Agile methodology to achieve an efficient 1-week sprint: daily standup and in-person interactions, and flexible course of actions to ensure working software.'
   ],
   skills: ['Ruby on Rails', 'PostgreSQL', 'HTML | CSS | JS', 'Heroku', 'GitHub', 'Bootstrap', 'jQuery', 'Cloudinary', 'Devise authentication', 'Geocoding', 'Mapbox API', 'Figma'], 
   demo: "https://petnow.herokuapp.com/",
