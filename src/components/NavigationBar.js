96ff03cc17a627ae4d63695257939b45b39783fe
Khoa Thien Le (Harry) <khoatle165@gmail.com>
2020-02-29 00:03:24 -0600

    fix bug that navbar appears as default in mobile

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import {Responsive} from 'semantic-ui-react';
+import { Responsive } from 'semantic-ui-react';
 import scrollIntoView from 'smooth-scroll-into-view-if-needed';
 import NavigationBarMobile from './NavigationBarMobile';
 import NavigationBarDesktop from './NavigationBarDesktop';
 import '../styles/NavigationBar.scss';
 
 const items = [
-  {text: 'Home', iconName: 'home', link: '/'},
-  {text: 'About', iconName: 'info', link: '/#about'},
-  {text: 'Projects', iconName: 'code', link: '/#projects'},
-  {text: 'Experiences', iconName: 'cogs', link: '/#experiences'},
-  {text: 'Education', iconName: 'student', link: '/#education'},
-  {text: 'Skills', iconName: 'lightbulb', link: '/#skills'},
-  // {text: 'Interests', iconName: 'chess', link: '/#interests'},
-  {text: 'Resume', iconName: 'tasks', link: '/#resume'},
-  {text: 'Contact', iconName: 'conversation', link: '/#contact'}
+  { text: 'Home', iconName: 'home', link: '/' },
+  { text: 'About', iconName: 'info', link: '/#about' },
+  { text: 'Projects', iconName: 'code', link: '/#projects' },
+  { text: 'Experiences', iconName: 'cogs', link: '/#experiences' },
+  { text: 'Education', iconName: 'student', link: '/#education' },
+  { text: 'Skills', iconName: 'lightbulb', link: '/#skills' },
+  { text: 'Resume', iconName: 'tasks', link: '/#resume' },
+  { text: 'Contact', iconName: 'conversation', link: '/#contact' }
 ];
 
 class NavigationBar extends React.Component {
-  state = {visible: true}
+  state = { visible: false };
   handlePusher = () => {
     if (this.state.visible) {
       this.setState({ visible: false });
@@ -27,9 +26,9 @@ class NavigationBar extends React.Component {
   handleToggle = () => this.setState({ visible: !this.state.visible });
   handleScreenSizeUpdate = (e, { width }) => {
     if (width >= Responsive.onlyTablet.minWidth) {
-      this.setState({visible: true});
+      this.setState({ visible: true });
     } else {
-      this.setState({visibile: false});
+      this.setState({ visibile: false });
     }
   };
   goToSection = (e, sectionName) => {
@@ -41,13 +40,14 @@ class NavigationBar extends React.Component {
         block: 'start',
         inline: 'nearest',
         duration: window.innerWidth > 499 ? 100 : 1500,
-        ease: t => t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t
+        ease: t =>
+          t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t
       });
     }
   };
   render() {
     return (
-      <div id="navigation-bar">
+      <div id='navigation-bar'>
         <Responsive {...Responsive.onlyMobile}>
           <NavigationBarMobile
             items={items}
@@ -55,7 +55,8 @@ class NavigationBar extends React.Component {
             onToggle={this.handleToggle}
             visible={this.state.visible}
             onUpdate={this.handleScreenSizeUpdate}
-            goToSection={this.goToSection}>
+            goToSection={this.goToSection}
+          >
             {this.props.children}
           </NavigationBarMobile>
         </Responsive>
@@ -66,18 +67,22 @@ class NavigationBar extends React.Component {
             onToggle={this.handleToggle}
             visible={this.state.visible}
             onUpdate={this.handleScreenSizeUpdate}
-            goToSection={this.goToSection}>
+            goToSection={this.goToSection}
+          >
             {this.props.children}
           </NavigationBarMobile>
         </Responsive>
-        <Responsive minWidth={Responsive.onlyComputer.minWidth} onUpdate={this.handleScreenSizeUpdate}>
+        <Responsive
+          minWidth={Responsive.onlyComputer.minWidth}
+          onUpdate={this.handleScreenSizeUpdate}
+        >
           <NavigationBarDesktop items={items} goToSection={this.goToSection}>
             {this.props.children}
           </NavigationBarDesktop>
         </Responsive>
       </div>
     );
-  };
+  }
 }
 
 export default NavigationBar;
