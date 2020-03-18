0aef2cac02adeb74eca2c5053bc461cf683a8014
Khoa Thien Le (Harry) <khoatle165@gmail.com>
2020-03-18 14:12:12 -0500

    add future experience at Pearson VUE

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,41 +1,54 @@
-import React from 'react';
-import {Row, Col} from 'reactstrap';
+import React, { Fragment } from 'react';
+import { Row, Col } from 'reactstrap';
 import '../styles/Experiences.scss';
 
 class Experience extends React.Component {
   render() {
     return (
-      <div className="experience-card">
+      <div className='experience-card'>
         <Row>
-          <Col xs="11" lg="3" className="experience-info">
-            <p className="experience-title">{this.props.title}</p>
-            <p className="experience-location">{this.props.location}</p>
-            <p className="experience-date">{this.props.date}</p>
+          <Col xs='11' lg='3' className='experience-info'>
+            <p className='experience-title'>{this.props.title}</p>
+            <p className='experience-location'>{this.props.location}</p>
+            <p className='experience-date'>{this.props.date}</p>
           </Col>
-          <Col xs="11" md="12" lg="8" className="experience-details">
-            <p className="experience-summary">{this.props.summary}</p>
-            <p className="bold">Key contributions:</p>
-            <ul>
-              {this.props.contributions.map((contribution, index) => {
-                return <li key={index}>{contribution}</li>
-              })}
-            </ul>
-            {this.props.projects ?
-              <p className="bold">Projects so far:</p>
-            : null}
-            {this.props.projects ?
+          <Col xs='11' md='12' lg='8' className='experience-details'>
+            {this.props.summary ? (
+              <p className='experience-summary'>{this.props.summary}</p>
+            ) : null}
+
+            {this.props.contributions ? (
+              <Fragment>
+                <p className='bold'>Key contributions:</p>
+                <ul>
+                  {this.props.contributions.map((contribution, index) => {
+                    return <li key={index}>{contribution}</li>;
+                  })}
+                </ul>
+              </Fragment>
+            ) : null}
+
+            {this.props.projects ? (
+              <p className='bold'>Projects so far:</p>
+            ) : null}
+            {this.props.projects ? (
               <ul>
                 {this.props.projects.map((project, index) => {
-                  return <li key={index}>{project}</li>
+                  return <li key={index}>{project}</li>;
                 })}
               </ul>
-            : null}
-            <p className="bold">Knowledge/Skills:</p>
-            <ul>
-              {this.props.skills.map((skill, index) => {
-                return <li key={index}>{skill}</li>
-              })}
-            </ul>
+            ) : null}
+
+            {this.props.skills ? (
+              <Fragment>
+                <p className='bold'>Knowledge/Skills:</p>
+                <ul>
+                  {this.props.skills.map((skill, index) => {
+                    return <li key={index}>{skill}</li>;
+                  })}
+                </ul>
+              </Fragment>
+            ) : null}
           </Col>
         </Row>
       </div>
--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React from 'react';
 import scrollIntoView from 'smooth-scroll-into-view-if-needed';
-import {Container, Button} from "reactstrap";
+import { Container, Button } from 'reactstrap';
 import Experience from './Experience';
 import '../styles/Experiences.scss';
 
+const upcomingExperience = {
+  title: 'Incoming Software Engineering Intern',
+  location: 'Pearson VUE inc.',
+  date: 'May 2020 - Aug 2020',
+  summary: 'Will be updated once the internship starts.',
+  contributions: [],
+  projects: [],
+  skills: []
+};
+
 const mainExperience = {
-  title: 'Student Web & Application Developer',
-  location: 'UW-Madison', date: 'October 2019 - now',
-  summary: 'Provided more than 50 hours technical support to the Department of Medical Physics.',
+  title: 'Student Web Developer',
+  location: 'UW-Madison',
+  date: 'October 2019 - now',
+  summary:
+    'Provided more than 50 hours technical support to the Department of Medical Physics.',
   contributions: [
     'Designing, implementing and maintaining source code.',
     'Modifying, updating and improving the existing department website, content and application.',
@@ -28,47 +40,56 @@ const mainExperience = {
     'Ability to work in a fast-paced, time-sensitive team environment.',
     'Exceptional interpersonal skills and the ability to work with diverse populations.'
   ]
-}
+};
 
 const extraExperiences = [
-  {title: 'Residence Hall Math Tutor',
-  location: 'UW-Madison', date: 'September 2019 - now',
-  summary: 'Facilitating weekly individual and group tutoring to university housing residents taking Mathematics courses through Math 222 (Calculus II).',
-  contributions: [
-    'Helping students make academic progress, while at the same time promoting independent learning, problem solving skills, and self-reliance.',
-    'Working one-on-one or in small groups to help university housing residents gain confidence in their math skills.',
-    'Collaborating with Residence Life staff to promote and provide feedback regarding the hall math tutoring program.',
-    'Answering questions and/or explained general concepts to aid students in their overall comprehension of the course material, without giving away the answers.'
-  ],
-  skills: [
-    'Ability to provide individual or small group tutoring sessions.',
-    'Ability to engage students in a discussion of the course content.',
-    'Ability to identify and successfully adapt to different students\' learning styles.',
-    'Possess excellent communication skills, problem solving skills, and the ability to work with a diverse student body.',
-    'Ability to work independently.'
-  ]},
-  {title: 'Culinary Student Team Member',
-  location: 'UW-Madison', date: 'August 2019 - May 2019',
-  summary: 'Provided more than 250 hours of exceptional customer service with the goal of making guests feel welcome and satisfied.',
-  contributions: [
-    'Communicated effectively with other coworkers and supervisors.',
-    'Worked with other students and professional staff to learn kitchen and food prep techniques.',
-    'Provided a welcoming, respectful and efficient food service experience to customers.',
-    'Operated cash register/POS system.',
-    'Washed dishes and maintain a clean and inviting dining area.',
-    'Served hot and cold menu items; grill; stock and inventory food and supplies.'
-  ],
-  skills: [
-    'Ability to work collaboratively and independently.',
-    'Strong commitment and communication skills.',
-    'Attention to detail and organization.',
-    'Ability to creatively solve problems.'
-  ]}
+  {
+    title: 'Residence Hall Math Tutor',
+    location: 'UW-Madison',
+    date: 'September 2019 - now',
+    summary:
+      'Facilitating weekly individual and group tutoring to university housing residents taking Mathematics courses through Math 222 (Calculus II).',
+    contributions: [
+      'Helping students make academic progress, while at the same time promoting independent learning, problem solving skills, and self-reliance.',
+      'Working one-on-one or in small groups to help university housing residents gain confidence in their math skills.',
+      'Collaborating with Residence Life staff to promote and provide feedback regarding the hall math tutoring program.',
+      'Answering questions and/or explained general concepts to aid students in their overall comprehension of the course material, without giving away the answers.'
+    ],
+    skills: [
+      'Ability to provide individual or small group tutoring sessions.',
+      'Ability to engage students in a discussion of the course content.',
+      "Ability to identify and successfully adapt to different students' learning styles.",
+      'Possess excellent communication skills, problem solving skills, and the ability to work with a diverse student body.',
+      'Ability to work independently.'
+    ]
+  },
+  {
+    title: 'Culinary Student Team Member',
+    location: 'UW-Madison',
+    date: 'August 2019 - May 2019',
+    summary:
+      'Provided more than 250 hours of exceptional customer service with the goal of making guests feel welcome and satisfied.',
+    contributions: [
+      'Communicated effectively with other coworkers and supervisors.',
+      'Worked with other students and professional staff to learn kitchen and food prep techniques.',
+      'Provided a welcoming, respectful and efficient food service experience to customers.',
+      'Operated cash register/POS system.',
+      'Washed dishes and maintain a clean and inviting dining area.',
+      'Served hot and cold menu items; grill; stock and inventory food and supplies.'
+    ],
+    skills: [
+      'Ability to work collaboratively and independently.',
+      'Strong commitment and communication skills.',
+      'Attention to detail and organization.',
+      'Ability to creatively solve problems.'
+    ]
+  }
 ];
- 
+
 class Experiences extends React.Component {
-  state = {expandMoreExperience: false}
-  toggle = () => this.setState({expandMoreExperience: !this.state.expandMoreExperience});
+  state = { expandMoreExperience: false };
+  toggle = () =>
+    this.setState({ expandMoreExperience: !this.state.expandMoreExperience });
   goToExperiencesAndToggle = e => {
     e.preventDefault();
     const experiences = document.querySelector('#experiences');
@@ -77,31 +98,37 @@ class Experiences extends React.Component {
       block: 'start',
       inline: 'nearest',
       duration: window.innerWidth > 499 ? 100 : 1500,
-      ease: t => t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t
+      ease: t =>
+        t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t
     });
     this.toggle();
   };
   render() {
     return (
       <Container>
+        <Experience {...upcomingExperience} />
         <Experience {...mainExperience} />
-        {!this.state.expandMoreExperience ?
-          <p className="see-more-statement">If you are not bored yet, 
-            <Button size="sm" onClick={() => this.toggle()}>Expand</Button> 
+        {!this.state.expandMoreExperience ? (
+          <p className='see-more-statement'>
+            If you are not bored yet,
+            <Button size='sm' onClick={() => this.toggle()}>
+              Expand
+            </Button>
             to learn more about my other experiences.
           </p>
-        : null}
-        {this.state.expandMoreExperience ? 
-          extraExperiences.map((experience, index) => {
-            return <Experience {...experience} key={index} />
-          })
-        : null}
-        {this.state.expandMoreExperience ? 
-          <a href="#experiences" onClick={this.goToExperiencesAndToggle}>See less</a>
-        : null}
+        ) : null}
+        {this.state.expandMoreExperience
+          ? extraExperiences.map((experience, index) => {
+              return <Experience {...experience} key={index} />;
+            })
+          : null}
+        {this.state.expandMoreExperience ? (
+          <a href='#experiences' onClick={this.goToExperiencesAndToggle}>
+            See less
+          </a>
+        ) : null}
       </Container>
-        
-    )
+    );
   }
 }
 
